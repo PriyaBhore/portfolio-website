b82d015eeaf65c48ac20fb0ed5f6689a100b3641
PriyaProjects <priyababanbhore@gmail.com>
2025-01-23 19:16:46 +0530

    Updated intro.jsx and Layout.jsx; added css and headerimages folders

--- src/section/header/intro.jsx
+++ src/section/header/intro.jsx
@@ -1,7 +1,20 @@
+import logoImage from './headerimages/logoimage.jpg';
+import './css/header.css';
+import './css/routing.css'
+
+
 function Intro(){
     return(
         <>
-        <p>This is introduction page</p>
+        <div className="header-container">
+            <h4>Priya Bhore- Portfolio</h4>
+            <div className="header-logo"> 
+            <img src={logoImage} alt="Logo" />
+            <h3>EroIT Solutions</h3>
+            {/* <p>A Complete solution to your task</p> */}
+             </div>
+        </div>
+
         </>
     );
 }
--- src/section/header/pages/Layout.jsx
+++ src/section/header/pages/Layout.jsx
@@ -3,14 +3,14 @@ import { Outlet,Link } from "react-router";
 function Layout(){
     return(
         <>
-            <nav>
-                <ul>
-                    <li><Link to='/Home'>Home</Link></li>
-                    <li><Link to='/Aboutme'>About Me</Link></li>
-                    <li><Link to='/Skills'>Skills</Link></li>
-                    <li><Link to='/Projects'>Project</Link></li>
-                    <li><Link to='/Experience'>Experience</Link></li>
-                    <li><Link to='/Contact'>Contact</Link></li>
+            <nav className="navbar">
+                <ul className="navbar-list">
+                    <li><Link to='/Home' className="navbar-item">Home</Link></li>
+                    <li><Link to='/Aboutme' className="navbar-item">About Me</Link></li>
+                    <li><Link to='/Skills' className="navbar-item">Skills</Link></li>
+                    <li><Link to='/Projects' className="navbar-item">Project</Link></li>
+                    <li><Link to='/Experience' className="navbar-item">Services</Link></li>
+                    <li><Link to='/Contact' className="navbar-item">Contact</Link></li>
 
 
                 </ul>
